63be1a28d70cd0c4cf0b0a8d88c621104da4352b
Shikhar Dixit <shikhardixit17@gmail.com>
2022-04-14 23:10:29 -0700

    logging to see whats going on

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,6 +6,7 @@ export const getUser = () =>
 const setUser = user =>
   window.localStorage.setItem("gatsbyUser", JSON.stringify(user))
 export const handleLogin = (password) => {
+  console.log(process.env);
   if (password === process.env.REACT_APP_CUSTOM_ENV_VAR) {
     setUser({
       name: `Palak 💖`
