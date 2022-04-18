cc12a0dd9d69b10cc89e6b4fbe0f74693cc8f0a1
Shikhar Dixit <shikhardixit17@gmail.com>
2022-04-17 21:36:58 -0700

    Pushing up content

--- src/constants/postListData.js
+++ src/constants/postListData.js
@@ -5,7 +5,8 @@ export const postListData = {
     blogPosts: [
       {
         date: "May, 2018",
-        description: "When we first met.When we first met.When we first met.When we first met.When we first met.When we first met.",
+        description:
+          "The time I really fell hard for you. Saw your fun-loving, outgoing side in its entiriety and enjoyed it a lot.",
         image: {
           fileName: "2018PostCard.jpg",
           title: "2018",
@@ -13,10 +14,30 @@ export const postListData = {
         },
       },
       {
-        date: "May, 2018",
-        description: "When we first met.",
+        date: "June, 2018",
+        description:
+          "New York!! One of my favorite visits ever. I was crazed that you knew so much about the ins and outs of Brooklyn and seeing your school was so interesting.",
         image: {
-          fileName: "2018PostCard.jpg",
+          fileName: "2018JuneNY.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "September, 2018",
+        description:
+          "Your first time at Rafis and one of our earliest dates :).",
+        image: {
+          fileName: "2018SeptemberRafis.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "November, 2018",
+        description: "ASIA Show when we danced together :D.",
+        image: {
+          fileName: "2018NovemberASIA.jpg",
           title: "2018",
           altTitle: "2018!",
         },
@@ -27,96 +48,188 @@ export const postListData = {
     id: 2,
     year: 2019,
     blogPosts: [
-        {
-          date: "May, 2018",
-          description: "When we first met.When we first met.When we first met.When we first met.When we first met.When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      {
+        date: "March, 2019",
+        description:
+          "Buffalo! We started the year off right :). Many steps in the snow haha.",
+        image: {
+          fileName: "2019MarchBuffalo.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-        {
-          date: "May, 2018",
-          description: "When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      },
+      {
+        date: "March, 2019",
+        description:
+          "More Buffalo! I remember eating Indian food and we also went to melting pot right next to the mall  which hit juuust right.",
+        image: {
+          fileName: "2019MarchBuffalo2.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-      ],
+      },
+      {
+        date: "April, 2019",
+        description:
+          "Shay Shay Shay. Very interesting times haha. I would be over a lot and it helped us get a lot closer. Seeing you grind at Shay and our many hickey visits were very memorable.",
+        image: {
+          fileName: "2019MarchShay.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "June, 2019",
+        description: "Going out at Bonas was always a blast!!",
+        image: {
+          fileName: "2019MarchOut.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "October, 2019",
+        description:
+          "Halloween!!",
+        image: {
+          fileName: "2019OctoberHalloween.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "October, 2019",
+        description: "Elicotville, super fun trip to go around and see all the booths. Made us realize we both love places with booths like farmers markets :).",
+        image: {
+          fileName: "2019OctoberElicotville.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+    ],
   },
   twentyTwenty: {
     id: 3,
     year: 2020,
     blogPosts: [
-        {
-          date: "May, 2018",
-          description: "When we first met.When we first met.When we first met.When we first met.When we first met.When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      {
+        date: "October, 2020",
+        description: "Fun at the beach! Miss this time, it was so relaxing. Felt like it was just us. Just gotta drive at speed limit 🚨 haha.",
+        image: {
+          fileName: "2020OctoberBradentonBeach.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-        {
-          date: "May, 2018",
-          description: "When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      },
+      {
+        date: "October, 2020",
+        description: "Tampa!! I'm glad we got to the biggest aquariam in the states!",
+        image: {
+          fileName: "2020OctoberTampa.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-      ],
+      },
+    ],
   },
   twentyTwentyOne: {
     id: 4,
     year: 2021,
     blogPosts: [
-        {
-          date: "May, 2018",
-          description: "When we first met.When we first met.When we first met.When we first met.When we first met.When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      {
+        date: "February, 2021",
+        description:
+          "Big Cat Habitat. Such an unique show. I was amazed by the ability to handle the big cats! Super fun :) We got our chance to go to Celery Fields later too!",
+        image: {
+          fileName: "2021FebruaryBigCatHabitat.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-        {
-          date: "May, 2018",
-          description: "When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      },
+      {
+        date: "February, 2021",
+        description:
+          "Maharaja!! We are big foodies.",
+        image: {
+          fileName: "2021FebruaryMaharaja.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-      ],
+      },
+      {
+        date: "February, 2021",
+        description:
+          "Golden Dinosaur!! More foodies stuffs.",
+        image: {
+          fileName: "2021FebruaryGoldenDinosaur.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "July, 2021",
+        description:
+          "July 4th!! The beach was super lively that day. And the fireworks were out of this world.",
+        image: {
+          fileName: "2021July4th.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "July, 2021",
+        description:
+          "Orlando was super fun. We went to Universal all over and spent time at half the resorts available. Harry Potter World lived up to its name super strongly!! And I loved seeing you nerd out over it :).",
+        image: {
+          fileName: "2021JulyOrlando.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+      {
+        date: "Goofy Pic 🤪",
+        description:
+          "I love the fact you have a goofy side that not many people say but it's so enjoyable and funny you have no idea.",
+        image: {
+          fileName: "2021GoofyPic.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      }
+    ],
   },
   twentyTwentyTwo: {
     id: 5,
     year: 2022,
     blogPosts: [
-        {
-          date: "May, 2018",
-          description: "When we first met.When we first met.When we first met.When we first met.When we first met.When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      {
+        date: "January, 2022",
+        description:
+          "Home sweet home. Spending lot of time with you at your home I always felt at home too.",
+        image: {
+          fileName: "2022JanuaryHome.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-        {
-          date: "May, 2018",
-          description: "When we first met.",
-          image: {
-            fileName: "2018PostCard.jpg",
-            title: "2018",
-            altTitle: "2018!",
-          },
+      },
+      {
+        date: "February, 2022",
+        description: "Florida State Fair exceeded all my expectations. I'm so glad we went. The birds we saw, the performances, the ferris wheel, the food..all of it.",
+        image: {
+          fileName: "2022FebruaryFloridaStateFair.jpg",
+          title: "2018",
+          altTitle: "2018!",
         },
-      ],
+      },
+      {
+        date: "February, 2022",
+        description: "Someone made some dank food (Pals did!) and it was super tasty. You made Valentine's day special especially by clutching out that restaurant. Note to self: be more vigilant!!",
+        image: {
+          fileName: "2022FebruaryFood.jpg",
+          title: "2018",
+          altTitle: "2018!",
+        },
+      },
+    ],
   },
 }
